d72948df57d898d92622a654a86e8f03a28b160d
Vitor <vitormiglianivpn@gmail.com>
2025-06-13 23:20:20 -0300

    Conexao com Banco de Dados

---
+++ DataBase/DataBase.php
@@ -0,0 +1,29 @@
+<?php 
+    class DataBase{
+        private $dbHost = "localhost";
+        private $dbUser = "root";        
+        private $dbPass = "";
+        private $dbName = "WorldCup";
+        public static $conn = null;
+
+        private function __construct(){
+            $this->dbHost;
+            $this->dbUser;
+            $this->dbPass;
+            $this->dbName;
+            if(self::$conn === null){
+                self::$conn = $this->connect();
+            }
+        }
+    
+        private function connect():PDO{
+            try {
+                $conn = new PDO("mysql:host={$this->dbHost};dbname={$this->dbName}", $this->dbUser, $this->dbPass);
+                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+                return $conn;
+            } catch (PDOException $e) {
+                die("Falha na conexão: " . $e->getMessage());
+            }
+    }
+}
